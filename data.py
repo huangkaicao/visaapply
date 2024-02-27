5ce319dba9654490a50ca93e4a68133fc525ca76
huang <huangkaicao@gmail.com>
2024-02-27 23:50:26 +0900

    部分文本框改下拉框，删除json中一个无用数据

--- data.py
+++ data.py
@@ -9,6 +9,31 @@ data_path = os.path.join(script_directory, "json", "data.json")
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
+    with open(data_path, 'r') as file:
+        data = json.load(file)
+    dropdown_fields = {
+        'first_page.location': ["CHINA, BEIJING", "CHINA, GUANGZHOU", "JAPAN, OSAKA/FUKUOKA", "JAPAN, TOKYO/SAPPORO"],
+        'second_page.security_question': ["What is the given name of your father's father?", "What is the given name of your mother's mother?"],
+        'personal_one.marital_status': ["SINGLE", "MARRIED", "DIVORCED"],
+        'personal_one.day': ["01", "02", "03"],
+        'personal_one.month': ["JAN", "FEB", "MAR"],
+        'personal_one.country': ["CHINA", "KOREA, REPUBLIC OF (SOUTH)", "JAPAN"],
+        'personal_two.country': ["CHINA", "KOREA, REPUBLIC OF (SOUTH)", "JAPAN"],
+        'travel.purpose_of_trip': ["TEMP. BUSINESS PLEASURE VISITOR (B)", "ACADEMIC OR LANGUAGE STUDENT (F)"],
+        'travel.specify': ["BUSINESS & TOURISM (TEMPORARY VISITOR) (B1/B2)", "BUSINESS/CONFERENCE (B1)"],
+        'travel.day': ["01", "02", "03"],
+        'travel.month': ["JAN", "FEB", "MAR"],
+        'travel.time_unit': ["Day(s)", "Week(s)", "Month(s)"],
+        'travel.state': ["WASHINGTON", "TENNESSEE", "KANSAS"],
+        'travel.paying': ["Self", "Present Employer", "Other Person"],
+        'travel.paying': ["Self", "Present Employer", "Other Person"],
+        'address_and_phone.country': ["CHINA", "KOREA, REPUBLIC OF (SOUTH)", "JAPAN"],
+        'address_and_phone.social_media_name': ["QZONE (QQ)", "FACEBOOK", "INSTAGRAM"],
+        'passport_info.type': ["REGULAR", "OFFICIAL"],
+        'passport_info.country': ["CHINA", "KOREA, REPUBLIC OF (SOUTH)", "JAPAN"],
+        'passport_info.day': ["01", "02", "03"],
+        'passport_info.month': ["JAN", "FEB", "MAR"],
+    }
     if request.method == 'POST':
         updated_data = request.form.to_dict()
         with open(data_path, 'r') as file:
@@ -25,7 +50,7 @@ def form():
     else:
         with open(data_path, 'r') as file:
             data = json.load(file)
-        return render_template('form.html', data=data)
+        return render_template('form.html', data=data,dropdown_fields=dropdown_fields)
 
 
 app.run()
